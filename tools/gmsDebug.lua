beeab8528df5c2c404277818e0d58cdb635896dc
Martin Eller <martin@eller-essen.de>
2021-05-12 13:16:16 +0200

    Update gmsDebug.lua

--- tools/gmsDebug.lua
+++ tools/gmsDebug.lua
@@ -1,18 +1,27 @@
 --
 -- Glowins Modschmiede: Debug-Tool
--- V0.9
+-- V0.9.3
 
 GMSDebug = {}
+GMSDebug.modName = "Unknown Mod"
+GMSDebug.state = false
+GMSDebug.consoleCommands = false
 
-function GMSDebug:init(modName, modPath, forceDbg)
+function GMSDebug:init(modName, forceDbg)
 	GMSDebug.modName = modName
-	GMSDebug.modPath = modPath
 	GMSDebug.state = (forceDbg == true)
+end
+
+function GMSDebug:enableConsoleCommands(doit)
+	if not doit then return; end
+	addConsoleCommand("gmsDebug", "Glowins Mod Smithery: Toggle Debug settings", "toggleDebug", GMSDebug)
+	addConsoleCommand("gmsPrint", "Glowins Mod Smithery: Debug printing", "consolePrint", GMSDebug)
+	GMSDebug:print("Debug Console Commands added")
 end
 
 function GMSDebug:print(text)
 	if not GMSDebug.state then return; end
-	print(modName.." :: "..text)
+	print(GMSDebug.modName.." :: "..text)
 end
 
 function GMSDebug:print_r(table)
@@ -24,24 +33,23 @@ end
 
 function GMSDebug:toggleDebug()
 	GMSDebug.state = not GMSDebug.modState
-	print("GMSDebug: New state is "..tostringGMSDebug.state)
+	print("GMSDebug: New state is "..tostring(GMSDebug.state))
 end
-addConsoleCommand("gmsDebug", "Glowins Mod Smithery: Toggle Debug settings", "toggleDebug", GMSDebug)
+
 
 function GMSDebug:consolePrint(object)
-	print(modName.." :: BEGIN of "..tostring(object).." =================")
+	print(GMSDebug.modName.." :: BEGIN of "..tostring(object).." =================")
 	print_r(object)
-	print(modName.." :: END of "..tostring(object).." =================")
+	print(GMSDebug.modName.." :: END of "..tostring(object).." =================")
 end
-addConsoleCommand("gmsPrint", "Glowins Mod Smithery: Debug printing", "consolePrint", GMSDebug)
 
 --
 
-function dbgPrint(text)
+function dbgprint(text)
 	GMSDebug:print(text)
 end
 
-function dbgPrint_r(table)
+function dbgprint_r(table)
 	GMSDebug:print_r(table)
 end
 
