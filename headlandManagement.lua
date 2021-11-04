f568df10ad7dd054eec158252d3ef2c5ec5c54a7
Martin Eller <martin@eller-essen.de>
2021-11-05 00:19:17 +0100

    1.1.2.0
    Hundegangsteuerung verbessert
    Ridgemarker-Lua behoben? --> Erster Lösungsansatz

--- headlandManagement.lua
+++ headlandManagement.lua
@@ -2,7 +2,7 @@
 -- Headland Management for LS 19
 --
 -- Jason06 / Glowins Modschmiede
--- Version 1.1.1.1
+-- Version 1.1.2.0
 --
 
 source(g_currentModDirectory.."tools/gmsDebug.lua")
--- headlandManagementRegister.lua
+++ headlandManagementRegister.lua
@@ -2,7 +2,7 @@
 -- Register Headland Management for LS 19
 --
 -- Jason06 / Glowins Modschmiede 
--- Version 1.1.1.1
+-- Version 1.1.2.0
 --
 
 function addHLMconfig(xmlFile, superfunc, baseXMLName, baseDir, customEnvironment, isMod, storeItem)
