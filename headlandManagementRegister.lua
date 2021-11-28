e5a2558ceead687816e3100e0823c4c08d4cfc56
Martin Eller <martin@eller-essen.de>
2021-11-28 23:45:42 +0100

    Update headlandManagementRegister.lua

--- headlandManagementRegister.lua
+++ headlandManagementRegister.lua
@@ -32,7 +32,7 @@ function addHLMconfig(xmlFile, superfunc, baseXMLName, baseDir, customEnvironmen
         	{name = g_i18n:getText("text_HLM_notInstalled_short"), index = 1, isDefault = true,  price = 0, dailyUpkeep = 0, desc = g_i18n:getText("text_HLM_notInstalled")},
         	{name = g_i18n:getText("text_HLM_installed_short"), index = 2, isDefault = false, price = 3000, dailyUpkeep = 0, desc = g_i18n:getText("text_HLM_installed")}
     	}
-    	StoreItemUtil.addConfigurationItem(configurations["HeadlandManagement"])
+    	--StoreItemUtil.addConfigurationItem(configurations["HeadlandManagement"])
     	dbgprint("addHLMconfig : Configuration HeadlandManagement added", 2)
     	dbgprint_r(configurations["HeadlandManagement"], 3)
 	end
