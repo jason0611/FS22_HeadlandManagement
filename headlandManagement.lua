74a13754f295ef47e9a6caa93908891917902479
Martin Eller <martin@eller-essen.de>
2021-11-30 12:09:03 +0100

    1.9.0.1

--- headlandManagement.lua
+++ headlandManagement.lua
@@ -2,7 +2,7 @@
 -- Headland Management for LS 22
 --
 -- Jason06 / Glowins Modschmiede
--- Version 1.9.0.0
+-- Version 1.9.0.1
 --
 
 source(g_currentModDirectory.."tools/gmsDebug.lua")
--- headlandManagementRegister.lua
+++ headlandManagementRegister.lua
@@ -2,7 +2,7 @@
 -- Register Headland Management for LS 22
 --
 -- Jason06 / Glowins Modschmiede 
--- Version 1.9.0.0
+-- Version 1.9.0.1
 --
 
 function addHLMconfig(xmlFile, superfunc, baseXMLName, baseDir, customEnvironment, isMod, storeItem)
