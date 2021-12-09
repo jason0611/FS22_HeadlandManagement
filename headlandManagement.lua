ff568bfea0dbbfce23866898b16a3a43f0800229
Martin Eller <martin@eller-essen.de>
2021-12-09 22:07:07 +0100

    Update headlandManagement.lua

--- headlandManagement.lua
+++ headlandManagement.lua
@@ -9,7 +9,7 @@
 
 
 source(g_currentModDirectory.."tools/gmsDebug.lua")
-GMSDebug:init(g_currentModName, true, 2)
+GMSDebug:init(g_currentModName, true, 1)
 GMSDebug:enableConsoleCommands("hlmDebug")
 
 source(g_currentModDirectory.."gui/HeadlandManagementGui.lua")
