58798423b24ca133b5adba76ac693e528ee5c534
Martin Eller <martin@eller-essen.de>
2022-01-30 11:58:42 +0100

    2.9.4.0
    ModHub RC

--- gui/HeadlandManagementGui.lua
+++ gui/HeadlandManagementGui.lua
@@ -2,7 +2,7 @@
 -- Headland Management for LS 22
 --
 -- Jason06 / Glowins Modschmiede
--- Version 2.9.3.7
+-- Version 2.9.4.0
 --
 
 HeadlandManagementGui = {}
--- headlandManagement.lua
+++ headlandManagement.lua
@@ -2,7 +2,7 @@
 -- Headland Management for LS 22
 --
 -- Jason06 / Glowins Modschmiede
--- Version 2.9.3.7
+-- Version 2.9.4.0
 --
 -- Make Headland Detection independent from other mods like GS
 -- Two nodes: front node + back node
