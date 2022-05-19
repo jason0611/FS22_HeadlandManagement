159d10630ec09a2285b87edd9036e7c3ccea8518
Martin Eller <martin@eller-essen.de>
2022-05-19 18:37:17 +0200

    2.b.1.2
    2.1.1.2 beta

--- headlandManagementRegister.lua
+++ headlandManagementRegister.lua
@@ -2,7 +2,7 @@
 -- Register Headland Management for LS 22
 --
 -- Jason06 / Glowins Modschmiede 
--- Version 2.b.1.1
+-- Version 2.1.1.2 beta
 --
 
 local specName = g_currentModName..".HeadlandManagement"
